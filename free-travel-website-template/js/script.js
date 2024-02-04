e785dc66ad1ca1c64a51c59f49e7f3dc457fd882
christopher <ojikac11@gmail.com>
2024-02-04 14:41:06 +0100

    uploaded site

---
+++ free-travel-website-template/js/script.js
@@ -0,0 +1,21 @@
+// const form = document.querySelector('card');
+
+// function sendEmail() {
+//     Email.send({
+//         Host : "smtp.elasticemail.com",
+//         Username : "g.altimate123@gmail.com",
+//         Password : "350F7DA7EFAC172B83B59020C35AFD2F3DD6",
+//         To : 'g.altimate123@gmail.com',
+//         From : "g.altimate123@gmail.com",
+//         Subject : "This is the subject",
+//         Body : "And this is the body"
+//     }).then(
+//       message => alert(message)
+//     );
+// }
+
+// card.addEventListener("submit", (e) => {
+//     e.preventDefault();
+
+//     sendEmail();
+// });
